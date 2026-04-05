6249cd3af9f8c1a3d4943c40077a5a74a2ee995c
Xinyi Zhang <xinyi.zhang@utsouthwestern.edu>
2026-04-05 17:12:55 -0500

    update 0405 with ACH payment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -150,9 +150,9 @@ SIMPLE_JWT = {
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.resend.com"
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "resend")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "re_HyGuLN45_5nh8QvKbuCAZvP6cJoZgtivG")
 DEFAULT_FROM_EMAIL = os.environ.get(
     "DEFAULT_FROM_EMAIL", "HXVP Studio <xinyi@hxvp.us>"
 )
